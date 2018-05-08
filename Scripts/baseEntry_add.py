8159f035eb74fc5a3e5c606ff4343c55329c5b3d
Alex <alex.milkis@kaltura.com>
2018-05-08 11:40:38 +0300

    Refactored the reconversion script

--- Scripts/baseEntry_add.py
+++ Scripts/baseEntry_add.py
@@ -21,36 +21,36 @@ client.setKs(ks)
 # The formatValue function collapses the result object and contructs a new json one
 def formatValue(val):
  if type(val) == list:
-	if val == []:
-	 return '[]'
-	result = '[\n'
-	for index in xrange(len(val)):
-	 result += '%s => %s\n' % (index, formatValue(val[index]))
-	result += ']'
-	return result
+  if val == []:
+   return '[]'
+  result = '[\n'
+  for index in xrange(len(val)):
+   result += '%s => %s\n' % (index, formatValue(val[index]))
+  result += ']'
+  return result
  elif 'PROPERTY_LOADERS' in dir(val): # object
-	result = '{\n'
-	for key, value in vars(val).items():
-	 result += '%s => %s\n' % (key, formatValue(value))
-	result += '}'
-	return result
+  result = '{\n'
+  for key, value in vars(val).items():
+   result += '%s => %s\n' % (key, formatValue(value))
+  result += '}'
+  return result
  elif 'getValue' in dir(val):   # enum
-	return val.getValue()
+  return val.getValue()
  else:
-	return val
+  return val
 
 def testUpdate(name):
-	# Create empty base entry object
-	baseEntry = KalturaBaseEntry()
-	# Define name argument to base entry object
-	baseEntry.name = name
-	baseEntry.type = "1"
-	type = KalturaEntryType.MEDIA_CLIP
-	# Save client add work to result object
-	result = client.baseEntry.add(baseEntry, type)
-	formattedResult = formatValue(result)
-	print (formattedResult)
-	return
+ # Create empty base entry object
+ baseEntry = KalturaBaseEntry()
+ # Define name argument to base entry object
+ baseEntry.name = name
+ baseEntry.type = "1"
+ type = KalturaEntryType.MEDIA_CLIP
+ # Save client add work to result object
+ result = client.baseEntry.add(baseEntry, type)
+ formattedResult = formatValue(result)
+ print (formattedResult)
+ return
 
 entryName = input('Enter name: ')
 testUpdate(entryName)
